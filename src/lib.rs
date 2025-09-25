26cbe4a9723491b929eead570c2beddf90ecd21e
Carson Price <carsondp02@gmail.com>
2025-09-25 14:42:40 -0500

    Stub library structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! travers: A graph library for flexible graph representations and algorithms.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod algo;
+pub mod core;
+pub mod prelude;
---
+++ src/prelude.rs
@@ -0,0 +1,3 @@
+pub use crate::algo::traversal::Bfs;
+pub use crate::core::graph::{DiGraph, UnGraph};
+pub use crate::core::traits::Graph;
